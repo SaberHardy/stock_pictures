ef9e059ad4fef120bc5263faaa86890516c6cf3a
SaberHardy <samirsamado5@gmail.com>
2022-03-27 23:29:40 +0300

    some configurations

--- stock_app/urls.py
+++ stock_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from stock_app import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('services/', views.services, name='services'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
--- stock_app/views.py
+++ stock_app/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'stock_app/index.html')
+
+
+def gallery(request):
+    return render(request, 'stock_app/gallery.html')
+
+
+def services(request):
+    return render(request, 'stock_app/services.html')
+
+
+def about(request):
+    return render(request, 'stock_app/about.html')
+
+
+def contact(request):
+    return render(request, 'stock_app/contact.html')
